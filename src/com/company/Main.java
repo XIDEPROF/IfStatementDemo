c56fbea422b84c73ff12f7f794dd05fd0414daaf
Serge <78102903+MrXides@users.noreply.github.com>
2021-11-01 16:22:38 +0300

    IfStatementDemo

---
+++ src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+    int a=2,b=3,c,d;
+
+        System.out.println("a = 2 , b =3 ");
+        if(a<b) System.out.println("а меньше b");
+        if(a>b) System.out.println("b меньше a");
+         System.out.println(" ");
+         c = a - b;
+        System.out.println("c = -1");
+        if(c>=0) System.out.println("c положительное число");
+        if(c<=0) System.out.println("с отрицательное число");
+    }
+}
